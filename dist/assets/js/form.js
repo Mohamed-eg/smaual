99a35a7cdf989279c7bde6af5cdfe458435a6163
Mohamed-eg <83299842+Mohamed-eg@users.noreply.github.com>
2023-01-29 19:55:59 +0200

    up8

---
+++ dist/assets/js/form.js
@@ -0,0 +1,43 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBwKzBNsdJzt5pQ5sEsQdB8CrI1RINYS50",
+    authDomain: "green-242f9.firebaseapp.com",
+    databaseURL: "https://green-242f9-default-rtdb.firebaseio.com",
+    projectId: "green-242f9",
+    storageBucket: "green-242f9.appspot.com",
+    messagingSenderId: "1034882225780",
+    appId: "1:1034882225780:web:6586f279d9974ac13be321"
+  };
+  // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+  var formDB = firebase.database().ref('contactForm') ;
+  const gitValue= (id)=>{
+      return document.getElementById(id).value;
+  }
+
+   const submitfu =(e)=>{
+      e.preventDefault() ;
+      var name =gitValue('Name');
+      var email =gitValue('email');
+      var phone =gitValue('phone');
+      var address =gitValue('address');
+      var country =gitValue('country');
+      var date =gitValue('date');
+      saveData(name,email,phone,address,country,date)
+      document.querySelector('.formcountaner').style.display="none";
+      document.querySelector('.valed').style.display="flex";
+  }
+
+  document.getElementById('contactForm').addEventListener("submit",submitfu);
+ 
+  //save the data
+  const saveData = (nam,email,phone,address,country,date)=>{
+    var newPerson = formDB.push();
+    newPerson.set({
+        name:nam,
+        email:email,
+        phone:phone,
+        address:address,
+        country:country,
+        date:date
+    })
+  }
